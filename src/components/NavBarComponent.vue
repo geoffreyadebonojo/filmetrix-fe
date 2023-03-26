1950f39d410af6ad34b08d7c5817ce5c18618182
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2023-03-26 16:02:11 -0600

    WIP

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -6,14 +6,13 @@
 </script>
 
 <template>
-  <input 
+  <input id="search-text"
     type="text" 
     placeholder="Search" 
-    id="search-text"
     tabindex="-1"
     @keyup.enter="submitSearch($event.target.value)"
   >
-  
+
   <div class="nav-button-container">
     <div id="highlight"></div>
  
@@ -21,43 +20,42 @@
       <img src="/search-icon.png" class="icon" id="search-icon">
     </div>
 
-    <div 
-      class="nav-button" id="person-button" v-if="displayPersonIcon() === true" >
+    <div id="person-button" class="nav-button" v-if="displayPersonIcon() === true" >
       <div @click="setCurrentFocus('person')">
         <img src="/person-icon.svg" class="icon" id="person-icon" >
       </div>
     </div>
     <div v-else></div>
 
-    <div class="nav-button" id="movie-button" v-if="displayMovieIcon() === true">
+    <div id="movie-button" class="nav-button" v-if="displayMovieIcon() === true">
       <div @click="setCurrentFocus('movie')">
         <img src="/movie-icon.svg" class="icon" id="movie-icon">
       </div>
     </div>
     <div v-else></div>
 
-    <div class="nav-button" id="tv-button" v-if="displayTvIcon() === true">
+    <div id="tv-button" class="nav-button" v-if="displayTvIcon() === true">
       <div @click="setCurrentFocus('tv')">
         <img src="/tv-icon.svg" class="icon" id="tv-icon">
       </div>
     </div>
 
-    <div class="nav-button" id="details-button" v-if="store.currentDetailId !== false" @click="setCurrentFocus('details')">
+    <div id="details-button" class="nav-button" v-if="store.currentDetailId !== false" @click="setCurrentFocus('details')">
       <img src="/details-icon.svg" class="icon" id="details-icon">
     </div>
     <div v-else></div>
 
-    <div class="nav-button" id="commands-button" @click="setCurrentFocus('commands')">
+    <div id="commands-button" class="nav-button" @click="setCurrentFocus('commands')">
       <img src="/command-icon.svg" class="icon" id="commands-icon">
     </div>
 
-    <router-link class="nav-button" id="about-button" to="/about">
+    <router-link id="about-button" class="nav-button" to="/about">
       <img src="/about-us-icon.svg" class="icon" id="about-us-icon">
     </router-link>
 
-    <div class="icon" id="back-button" @click="back()">
+    <!-- <div class="icon" id="back-button" @click="back()">
       <img src="/back-icon.svg" id="back-icon">
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -200,9 +198,15 @@ export default {
     },
 
     openField(d) {
-      d.transition().duration(0).delay(100)
-      .style("width", "100%")
-      .style("left", "7%")
+      if (store.isMobile) {
+        d.transition().duration(0).delay(100)
+        .style("width", "100%")
+        .style("left", "7%")
+      } else {
+        d.transition().duration(0).delay(100)
+        .style("width", "100%")
+        .style("left", "7%")
+      }
 
       d3.select("#highlight").transition().duration(100)
       .style("left", "-1px")
@@ -211,97 +215,114 @@ export default {
 }
 </script>
 
-<style scoped>
-  .nav-button-container {
-    height: 100%;
-    width: max-content;
-    display: flex;
-    justify-content: space-between;
-    right: 0px;
-    background: #6e6e6e;
-    border-radius: 15px 0 0 15px;
-  }
-  
-  #search-text {
-    width: 100%;
-    padding: 0px;
-    position: relative;
-    left: 20px;
-    height: 26px;
-    border-radius: 15px 0 0 15px;
-    border: 7px solid white;
-    text-align: center;
-    font-size: 15px;
-    letter-spacing: 0.05em;
-    box-sizing: border-box;
-    text-transform: uppercase;
-    font-family: 'Dosis', sans-serif;
-  }
+<style lang="scss">
+  #panel-body__desktop {
+    #navbar {
+      grid-area: navbar;
+      background: #333333;
+      display: flex;
+      margin: auto 0 auto auto;
+      height: 26px;
+      width: 100%;
 
-  #search-text:focus {
-    outline: none;
-  }
-    
-  .nav-button-container, #search-text{
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  }
+      #search-text {
+        width: 100%;
+        padding: 0px;
+        position: relative;
+        left: 20px;
+        height: 26px;
+        border-radius: 15px 0 0 15px;
+        border: 7px solid white;
+        text-align: center;
+        font-size: 15px;
+        letter-spacing: 0.05em;
+        box-sizing: border-box;
+        text-transform: uppercase;
+        font-family: 'Dosis', sans-serif;
 
-  #highlight {
-    position: absolute;
-    height: 100%;
-    width: 27px;
-    background: white;
-    border-radius: 50%;
-    right: 58px;
-  }
+        &:focus {
+          outline: none;
+        }
+      }
+      .nav-button-container {
+        grid-area: nav-button-container;
+        height: 100%;
+        width: max-content;
+        display: flex;
+        justify-content: space-between;
+        right: 0px;
+        background: #6e6e6e;
+        border-radius: 15px 0 0 15px;
 
-  #search-icon {
-    height: 17px;
-    margin: 4.5px 5.5px;
-  }
+        #highlight {
+          position: absolute;
+          height: 100%;
+          width: 27px;
+          background: white;
+          border-radius: 50%;
+          right: 58px;
+        }
 
-  #about-us-icon {
-    height: 17px;
-    margin: 4px 6px;
-  }
+        #search-icon {
+          height: 17px;
+          margin: 4.5px 5.5px;
+        }
 
-  #details-icon {
-    height: 27px;
-    left: 1.5px;
-  }
+        .nav-button, .result-button {
+          width: 28px;
+        }
 
-  #commands-icon {
-    height: 24px;
-    top: 0.25px;
-    left: 3px
-  }
+        .nav-button-container, #search-text{
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        }
 
-  #person-icon, #movie-icon, #tv-icon {
-    height: 16px;
-    top: 5px;
-  }
+        #person-icon {
+          height: 16px;
+          left: 7px;
+          top: 5px;
+        }
 
-  #person-icon, #tv-icon {
-    left: 8px;
-  }
+        #movie-icon {
+          height: 16px;
+          left: 7px;
+          top: 5px;
+        }
 
-  #movie-icon {
-    left: 6px;
-  }
+        #tv-icon {
+          height: 16px;
+          left: 8px;
+          top: 5px;
+        }
+        
+        #details-icon {
+          height: 27px;
+          left: 1.5px;
+        }
+        
+        #commands-icon {
+          height: 24px;
+          top: 0.25px;
+          left: 3px
+        }
 
-  .icon:hover {
-    cursor: pointer;
-  }
-  .nav-button, .result-button {
-    width: 28px;
-  }
+        #about-us-icon {
+          height: 17px;
+          margin: 4px 6px;
+        }
 
-  #back-button {
-    display: none;
-    top: 50px;
-    position: absolute;
-    height: 16px;
-    margin: 0px 10px 6px 10px;
-    transform: rotate(0deg);
+        .icon:hover {
+          cursor: pointer;
+        }
+        // #back-button {
+        //   display: none;
+        //   top: 50px;
+        //   position: absolute;
+        //   height: 16px;
+        //   margin: 0px 10px 6px 10px;
+        //   transform: rotate(0deg);
+        // }
+
+      }
+    }
   }
 </style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,253 @@
+<script setup>
+  // import PanelComponent from '../components/PanelComponent.vue'
+  // import * as d3 from 'd3'
+  import NavBarComponent from '../components/NavBarComponent.vue'
+  import PanelContentsComponent from '../components/PanelContentsComponent.vue'
+  import ControlsComponent from '../components/ControlsComponent.vue'
+  import { store } from '@/stores/store.js'
+  import * as d3 from 'd3'
+</script>
+
+<template>
+  <div v-bind:id="'panel-body__' + store.agent">
+  <!-- <div v-bind:id="store.agent + '-panel-body'"> -->
+    <img src="/center-graph-icon.svg" class="centering-button" id="centering-button" alt="centering button">
+
+    <div id="resize-bar" class="main-panel-component">
+      <div id="left-line"></div>
+      <div id="right-line"></div>
+    </div>
+
+    <div id="navbar">
+      <nav-bar-component></nav-bar-component>
+    </div>
+
+    <div id="panel-center" style="height:142%;zoom:100%">
+      <panel-contents-component></panel-contents-component>
+    </div>
+
+    <div id="controls" class="main-panel-component" style="transform: scale(1, 0); bottom: -50%;">
+      <controls-component></controls-component>
+    </div>
+
+    <!-- <img src="/settings-white.svg" class="icon" id="settings-icon" @click="this.toggleControls()"> -->
+    <!-- img v-else src="assets/settings-black.svg" class="icon" id="settings-icon" @click="this.toggleControls()"-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MainView",
+    data () {
+      return {
+        transitionTimer: 500
+      }
+    },
+    components: {
+      NavBarComponent,
+      PanelContentsComponent,
+      ControlsComponent
+    },
+    methods: {
+      toggleControls () {
+        if (store.showControls) {
+          //close
+          store.showControls = false
+
+          d3.select("#controls")
+          .transition().duration(this.transitionTimer)
+          .style("transform", "scaleY(0)")
+          .style("bottom", "-50%")
+
+          d3.select("#panel-center")
+          .transition().duration(this.transitionTimer)
+          .style("height", "142%")
+          
+        } else {
+          //open
+          store.showControls = true
+
+          d3.select("#controls")
+          .transition().duration(this.transitionTimer)
+          .style("transform", "scaleY(1)")
+          .style("bottom", "-0%")
+
+          d3.select("#panel-center")
+          .transition().duration(this.transitionTimer)
+          .style("height", "100%")
+        }
+      }
+    },
+    mounted () {
+      const panel = d3.select("#panel-body__desktop")
+      let isMobile = /Android|iPhone/i.test(navigator.userAgent)
+
+      panel.transition()
+      .duration(80)
+      .ease(d3.easeBounceOut)
+      .style("width", () => {
+        if (isMobile) {
+          return "290px"
+        } else {
+          return "350px"
+        }
+      })
+      .style("min-width", "270px")
+
+      const resizeBar = d3.select("#resize-bar")
+
+      resizeBar.call(
+        d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+      )
+
+      if (isMobile) {
+        resizeBar.on("click", () => {
+          let panel = d3.select("#panel-body__desktop")
+          let zoomButtons = d3.select("#zoom-buttons")
+          let centeringButton = d3.select("#centering-button")
+          
+          if (store.panelOpen) {
+            panel.transition().duration(100)
+            .style("width", "20px")
+            .style("min-width", "0px")
+            zoomButtons.style("display", "none")
+            centeringButton.style("display", "block")
+            store.panelOpen = false
+            
+          } else {
+            panel.transition()
+            .duration(80)
+            .ease(d3.easeBounceOut)
+            .style("width", "290px")
+            .style("min-width", "270px")
+            zoomButtons.style("display", "grid")
+            centeringButton.style("display", "none")
+            store.panelOpen = true
+          }
+        })
+      }
+
+      function dragstarted() {
+        d3.select(this).style("cursor", "col-resize")
+      }
+      
+      function dragged() {
+        d3.select(this).style("cursor", "col-resize")
+        let panel = this.parentElement
+        panel.style.width = `${window.innerWidth - event.x}px`
+      }
+
+      function dragended() {
+        store.panelWidth = window.innerWidth - event.x
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+  #panel-body {
+    &__desktop {
+      height: 100vh;
+      width: 0px;
+      display: grid;
+      grid-template-columns: 30px 1fr 30px;
+      grid-template-rows: 2vh 1.8em 4vh 10fr 1vh 4fr 4vh;
+      grid-template-areas:
+      "resize-bar . ."
+      "resize-bar navbar navbar"
+      "resize-bar . settings"
+      "resize-bar panel-center ."
+      "resize-bar . ."
+      "resize-bar controls ."
+      "resize-bar . .";
+      background: #333333;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      z-index: 2;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+      .centering-button {
+        background: none;
+        display: none;
+        color: white;
+        bottom: 10px;
+        width: 20px;
+        position: absolute;
+        z-index: 1;
+        cursor: pointer;
+      }
+
+      #resize-bar {
+        grid-area: resize-bar;
+        /* background: #444; */
+        width: 12px;
+
+        #left-line, #right-line {
+          position: relative;
+          border-left: 2px solid black;
+          top: 300px;
+          left: 3px;
+          height: 50px;
+        }
+      
+        #right-line {
+          left: 8px;
+          top: 250px;
+        }
+      }
+
+      #panel-center {
+        grid-area: panel-center;
+        overflow-y: auto;
+      }
+
+    }
+  }
+
+
+  
+  #zoom-buttons {
+    display: none;
+    position: absolute;
+    bottom: 40px;
+    right: 10px;
+    z-index: 5;
+    height: 80px;
+    justify-content: space-between;
+  }
+
+  #zoom-buttons > img {
+    width: 30px;
+    height: 30px;
+    background: white;
+    border-radius: 21%;
+  }
+
+  #instructions {
+    position : absolute;
+    top: 40%;
+    right: 0%;
+    z-index: 2
+  }
+
+
+
+
+
+  #settings-icon {
+    grid-area: setting;
+    height: 20px;
+    position: absolute;
+    top: 7vh;
+    right: -1px;
+    
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
