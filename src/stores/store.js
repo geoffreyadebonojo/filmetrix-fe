e757fbd7b22b65e12a923806e109c2d1a8c5692e
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2023-03-26 18:57:05 -0600

    store

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,13 +1,11 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  // these two clash
+  inMotion: false,
   currentFocus: 'empty',
   currentResultTab: '',
-  // prev: '',
   aboutUs: false,
   panelWidth: '350',
-  ///
   searchResults: [],
   currentDetailId: false,
   existing: [],
