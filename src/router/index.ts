f77fd2f9ba6712ed6bf269595f386b2d6c732cca
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2025-01-27 12:07:52 -0700

    Been meaning to fix that for a while

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeView from '../views/WelcomeView.vue'
 import MainView from '../views/MainView.vue'
-import GameView from '../views/GameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'welcome',
-      component: WelcomeView
+      name: 'root',
+      component: MainView
     },
     {
       path: '/graph',
       name: 'graph',
       component: MainView
-    },
-    // {
-    //   path: '/game',
-    //   name: 'game',
-    //   component: GameView
-    // }
+    }
   ]
 })
 
