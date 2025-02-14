500c55bf1e7d9d5cf46538e652083802efcfea24
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2025-02-14 09:41:00 -0700

    Cleanup, small changees

--- src/models/Simulation.js
+++ src/models/Simulation.js
@@ -1,3 +1,4 @@
+import { graphStates } from "@/stores/store.js"
 import * as d3 from 'd3'
 
 export default class Simulation {
@@ -6,70 +7,43 @@ export default class Simulation {
     this.nodes = nodes
     this.links = links
 
+    this.sim = d3.forceSimulation(this.nodes)
+
     this.body = this.generateGraph(
       this.fetchGraphAttrs(graphType)
     )
   }
 
-  // setting of forces still being researched
-  // ---- static or dynamic -----
-  // there was also ideas for slider control
-  // which would require dynamic control
-  // at the moment more trouble than its worth
-
-  // generateGraph(args) { 
-  //   const sim = d3.forceSimulation(this.nodes, this.links)
-  //   .velocityDecay(0.5)
-  //   .force("link", d3.forceLink(this.links).id(d => d.id).distance((link) => {
-  //     // return (link.target.popularity + link.source.popularity)/2
-  //     return 200
-  //   }).strength((link) => {
-  //     // return 1 / Math.min(count(link.source), count(link.target));
-  //     return 0.5
-  //   }))
-  //   .force("charge", d3.forceManyBody().strength((node) => {
-  //     // console.log(node.name, node.popularity)
-  //     return -2000
-  //   }))
-  //   .force('collide', d3.forceCollide(80).strength(1))
-  //   .force("center", d3.forceCenter(
-  //     ...args.forceCenter
-  //   ))
-  //   .force("radial", d3.forceRadial((d) => {
-  //     return 300, window.innerWidth/2, window.innerHeight/2
-  //   }))
-  //   .alpha(args.alpha.g)
-  //   .alphaTarget(args.alpha.target)
-    
-  //   if (args.alpha.min != null) {
-  //     sim.alphaMin(args.alpha.min)
-  //   }
-
-  //   return sim
-  // }
   generateGraph(args) { 
-    const sim = d3.forceSimulation(this.nodes, this.links)
-    .velocityDecay(0.8)
-    .force("link", d3.forceLink(this.links).id(d => d.id).distance((d) => {
-      // debugger
-      return 200
-    }))
-    .force("charge", d3.forceManyBody().strength(-2000))
-    .force('collide', d3.forceCollide(80))
-    .force("center", d3.forceCenter(
-      ...args.forceCenter
-    ))
-    .force("radial", d3.forceRadial((d) => {
-      return 600, 0, 0
-    }))
-    .alpha(args.alpha.g)
-    .alphaTarget(args.alpha.target)
+    this.sim.force("link", d3.forceLink(this.links).id(d => d.id).distance((d) => {
+              // seems like a helpful change
+              let data = graphStates.graphData[d.source.id] || graphStates.graphData[d.target.id]
+              let dist = data.links.filter(l => l.index).length * 6
+              return dist+200
+            }))
+            .force("charge", d3.forceManyBody().strength((d) => {
+              let c = -2000
+              // if (graphStates.graphData[d.id]) {
+              //   c = graphStates.graphData[d.id].links.filter(l => l.index).length * -20
+              // }
+              return c
+            }))
+            .force('collide', d3.forceCollide(60))
+            .force("center", d3.forceCenter(
+              ...args.forceCenter
+            ))
+            .force("radial", d3.forceRadial((d) => {
+              return 500, 0, 0
+            }))
+    
+    this.sim.alpha(args.alpha.g)
+            .alphaTarget(args.alpha.target)
     
     if (args.alpha.min != null) {
-      sim.alphaMin(args.alpha.min)
+      this.sim.alphaMin(args.alpha.min)
     }
 
-    return sim
+    return this.sim
   }
 
   fetchGraphAttrs(type){ 
